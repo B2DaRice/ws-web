9e0caa87ca45572e413b7f3b81f2ca07373d1fc3
Brice Garlick <brice@appdetex.com>
2017-10-30 08:52:36 -0600

    Using react-create-app as a starter for the project.  Have things up and running now, time to start branching for features.

---
+++ src/cmps/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+	render() {
+		return (
+			<div>
+				<header className="App-header">
+					<h1 className="App-title">Wookiee Serenade</h1>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default Header;
---
+++ src/cmps/Router/Router.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from "../Header/Header";
+
+class Router extends Component {
+	render() {
+		return (
+			<div>
+				<Header/>
+			</div>
+		);
+	}
+}
+
+export default Router;
